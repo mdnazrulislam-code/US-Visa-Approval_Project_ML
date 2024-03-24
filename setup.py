6f398e5e5275e32b62f62b0cb1bd5e129f76eea4
Md Nazrul Islam <arfanislamabir@gmail.com>
2024-03-24 22:03:19 +0600

    EDA and requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Nazrul",
+    author_email="arfanislamabir@gmail.com",
+    packages=find_packages()
+)
